8031580f0f822cbfae814511e2e1ce3ee3bb3088
Birdknothing <1213228277@qq.com>
2019-03-19 09:25:00 +0800

    upload find.js

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # fs-utils
-quick fs operation ( similar to linux command )
+quick fs operation ( similar to linux command );
+
+replace.js ( find str in particular file by extname or basename and replace it && transfer file to rename it );
+
+find.js ( like above but alert files' urls which contains the target str )
--- find.js
+++ find.js
@@ -14,78 +14,77 @@ var initialurl = './',
      * path.basename('fab/abc.html','.html')// abc
      * nameArr.includes('abc.js')// true
      * 转移文件用到此数组和fileNameOrStrToReplac和targetUrl
+     * 要寻找的字符串
      */
     nameArr = ['1.txt','3.txt','abc.js'],
     /**
      * 要替换的文件名
      */
-    nameToReplaceArr = ['11.txt','33.txt'],
+    // nameToReplaceArr = ['11.txt','33.txt'],
     /**
      * path.extname(url)//得到'.html','.js'这种
      */
-    extArr = ['.txt','.html'],
+    extArr = ['.js','.vue'],
     /**
      * 需要修改的字符串,相同下标的值自动替换
      * 
      */
-    strOriginArr = ['1','8'],
+    searchStrArr = ['1','8'];
     /**
      * 用以下字符串(新文件名）去替换
      * ！！注意，如果此数组内有值和strOriginArr数组内的值相同，则还是会被替换成strToReplaceArr里对应的最终值!!
      * 
      */
-    strToReplaceArr = ['2','4'],
+    // strToReplaceArr = ['2','4'],
     /**
      * 如果需要转移文件，目标文件夹路径
      */
-    targetUrl = './css/';
+    // targetUrl = './css/';
 
 // test searchToOperate()
 /**
  * 替换字符串，或转移文件并改名
  */
 var config = {
-    mode: 'nameArr',
-    //是否替换字符串
-    replaceStr: true,
+    mode: 'extArr'
+    // replaceStr: true,
     //是否转移并更名文件，需要数组内是文件名
-    renameTransfer: false
+    // renameTransfer: false
 }
 
 //功能之替换字符串
 searchToOperate(initialurl,config,(absurl)=>{
     //如果需要讲修改后的文件保存一份新的到另一个地址，需要第四个参数
-    if(config.replaceStr)
-    replaceStr(strOriginArr,strToReplaceArr,absurl);
+    // if(config.replaceStr)
+    findStr(searchStrArr,absurl);
 });
 
 //功能实现之转移文件并改名
-searchToOperate(initialurl,config,(absurl)=>{
-    //转移开始
-    if(config.renameTransfer)
-    transferRenameFile(absurl,nameToReplaceArr,targetUrl);
-})
+// searchToOperate(initialurl,config,(absurl)=>{
+//     //转移开始
+//     if(config.renameTransfer)
+//     transferRenameFile(absurl,nameToReplaceArr,targetUrl);
+// })
 
 //文件改名并复制到对应目录
-function transferRenameFile(originurl,nameToReplaceArr,targetUrl){
-    //找到要修改文件名对应的下标
-    var filename = path.basename(originurl);
-    var index = nameArr.indexOf(filename);
-    var toabsurl = path.resolve(targetUrl,nameToReplaceArr[index]);
-    var readable = fs.createReadStream(originurl);
-    var writable = fs.createWriteStream(toabsurl);
-    readable.pipe(writable);
-    console.log(originurl+' is renamed & replaced to '+toabsurl);
-}
+// function transferRenameFile(originurl,nameToReplaceArr,targetUrl){
+//     //找到要修改文件名对应的下标
+//     var filename = path.basename(originurl);
+//     var index = nameArr.indexOf(filename);
+//     var toabsurl = path.resolve(targetUrl,nameToReplaceArr[index]);
+//     var readable = fs.createReadStream(originurl);
+//     var writable = fs.createWriteStream(toabsurl);
+//     readable.pipe(writable);
+//     console.log(originurl+' is renamed & replaced to '+toabsurl);
+// }
 
 //定义替换字符串操作，前面两个参数是数组
-function replaceStr(nameOrigin,nameToReplace,originurl,targeturl){
+function findStr(nameOrigin,originurl){
     //省略第四个参数则默认在原文件修改
-    targeturl = targeturl || originurl;
     //!!!此处原始目录必须已经存在
     var absurl = path.resolve(originurl);
     //!!!此处目标目录不一定需要存在
-    var toabsurl = path.resolve(targeturl);
+    // var toabsurl = path.resolve(targeturl);
     var readable = fs.createReadStream(absurl);
     var arr = [];
     //每一次读取64kb的数据
@@ -102,24 +101,25 @@ function replaceStr(nameOrigin,nameToReplace,originurl,targeturl){
         nameOrigin.forEach((origStr,index)=>{
         //所有字符串
         var regexp = new RegExp(origStr,'g');
-        if(nameToReplace[index] === undefined)
-        {
-            throw new Error('要替换的字符串不足！');
-        }
+        // if(nameToReplace[index] === undefined)
+        // {
+        //     throw new Error('要替换的字符串不足！');
+        // }
         //找到匹配的再替换
         if(originStr.search(regexp) !== -1 )
         {
+            console.log(origStr+' was found in '+absurl);
             //不断修改原来的内容
-            originStr = originStr.replace(regexp,nameToReplace[index]);
-            console.log(absurl+' was found and replaced!');
+            // originStr = originStr.replace(regexp,nameToReplace[index]);
+            // console.log(absurl+' was found ');
         }
         //所有字符串都被替换完后
-        if(index === nameOrigin.length-1){
-            //此时可以安全创建写入流,注意end事情没有触发时是不可以同时创建写入流的,end触发后文件描述符自动关闭
-            var writable = fs.createWriteStream(toabsurl);
-            //写入文件
-            writable.write(originStr);
-        }
+        // if(index === nameOrigin.length-1){
+        //     //此时可以安全创建写入流,注意end事情没有触发时是不可以同时创建写入流的,end触发后文件描述符自动关闭
+        //     var writable = fs.createWriteStream(toabsurl);
+        //     //写入文件
+        //     writable.write(originStr);
+        // }
 
         })
 
